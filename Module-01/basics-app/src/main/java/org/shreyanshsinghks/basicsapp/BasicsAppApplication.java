f08ca4022929d87090cc3f840c733e9b1207645a
Shreyansh <shreyanshsinghks@gmail.com>
2026-01-26 17:39:39 +0530

    Module 1 Video 1.1 - Introduction to Spring Framework

---
+++ Module-01/basics-app/src/main/java/org/shreyanshsinghks/basicsapp/BasicsAppApplication.java
@@ -0,0 +1,13 @@
+package org.shreyanshsinghks.basicsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsAppApplication.class, args);
+    }
+
+}
---
+++ Module-01/basics-app/src/test/java/org/shreyanshsinghks/basicsapp/BasicsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shreyanshsinghks.basicsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
