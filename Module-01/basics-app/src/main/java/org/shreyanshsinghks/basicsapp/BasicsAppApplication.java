2cd6537bb797766766cb5bddf89f136db00521ac
Shreyansh <shreyanshsinghks@gmail.com>
2026-02-15 15:03:32 +0530

    Module 1 Video 1.2 - start

--- Module-01/basics-app/src/main/java/org/shreyanshsinghks/basicsapp/BasicsAppApplication.java
+++ Module-01/basics-app/src/main/java/org/shreyanshsinghks/basicsapp/BasicsAppApplication.java
@@ -8,7 +8,7 @@ public class BasicsAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicsAppApplication.class, args);
-//        No code to write in video 1.1
+//        Start of video 1.2
     }
 
 }
